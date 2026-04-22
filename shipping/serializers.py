d7d7f57a9dcff2da4336c00f43d3d4a543413b4f
emmanuelal99 <emma.aliu770@outlook.com>
2026-04-22 22:39:08 +0100

    Delete shipping directory

--- shipping/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-
-
-class ScannerPayloadSerializer(serializers.Serializer):
-    tracking_id = serializers.CharField(max_length=50)
-    location = serializers.CharField(max_length=255)
-    status = serializers.CharField(max_length=50)
-    description = serializers.CharField(max_length=255)
